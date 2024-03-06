f1c97a8d0978d97a11062ad284c52c80671ab5c8
Gonzalo <gonzaloblmnt@gmail.com>
2024-03-06 16:29:57 -0300

    first commit

---
+++ src/HooksApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ContadorComponent } from "./components/ContadorComponent";
+
+export const HookApp = () => {
+  return (
+    <>
+      <h1>Aplicacion de Hooks</h1>
+      <hr />
+      <ContadorComponent/>
+    </>
+  );
+};
